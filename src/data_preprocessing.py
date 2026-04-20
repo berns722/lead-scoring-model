6cebcb7836228ae4d14eb859f28f738f56acf652
berns722 <bernardohdezmoreno@gmail.com>
2026-04-20 17:34:07 -0600

    feat: add preprocessing step to remove ID column

--- src/data_preprocessing.py
+++ src/data_preprocessing.py
@@ -6,3 +6,8 @@ def head_and_tail(df, n=5):
     display(df.head(n))
     print("\nTail:")
     display(df.tail(n))
+
+def preprocess_data(df):
+    df = df.copy()
+    df = df.drop(columns=['ID'])  # Drop ID column
+    return df
